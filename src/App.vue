fb689611a52cd404ad3d0fc2714e061aa7065cdf
Julian L <julian.lawrance@gmail.com>
2023-11-19 15:09:11 +0000

    Create basic todo list functionality

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
-<template>
-    <h1>💖 Hello World!</h1>
-    <p>Welcome to your Electron application.</p>
-</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
 
-<script setup>
-console.log('👋 This message is being logged by "App.vue", included via Vite');
+interface Item { label: string, done: boolean }
+
+const items = ref([] as Item[])
+const isAddMode = ref(false)
+const newItemLabel = ref("")
+
+const addInput = ref<any>(null)
+
+watch(addInput, () => {
+    addInput.value?.focus()
+})
+
+const addNewItem = (e: Event) => {
+    e.preventDefault()
+
+    if (newItemLabel.value) {
+        items.value = [{
+            label: newItemLabel.value.trim(),
+            done: false,
+        }, ...items.value]
+
+        newItemLabel.value = ""
+    }
+}
 </script>
+
+<template>
+    <h1>To-Do List</h1>
+
+    <button v-if="!isAddMode" @click="isAddMode = true">Add new item</button>
+
+    <div v-else>
+        <form @submit="addNewItem">
+            <input ref="addInput" v-model="newItemLabel" />
+            <button :disabled="!newItemLabel" type="submit">Add</button>
+            <button type="reset" @click="isAddMode = false">Cancel</button>
+        </form>
+    </div>
+
+    <div>
+        <div v-if="items.length <= 0">No items!</div>
+
+        <div v-else v-for="(item, idx) in items">
+            <input :id="'input-' + idx" type="checkbox" v-model="item.done" />
+            <label :for="'input-' + idx">{{ item.label }}</label>
+        </div>
+    </div>
+</template>
