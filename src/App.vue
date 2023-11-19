00e228b25eeba2b9296e640ff4d90692b99a48ce
Julian L <julian.lawrance@gmail.com>
2023-11-19 15:59:30 +0000

    Allow hiding completed items

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 interface Item { label: string, done: boolean }
 
 const items = ref([] as Item[])
+const itemsToShow = computed(() => hideCompleted.value ? items.value.filter(i => !i.done) : items.value)
+const completedCount = computed(() => items.value.filter(i => i.done).length)
+
 const isAddMode = ref(false)
 const newItemLabel = ref("")
+
+const hideCompleted = ref(false)
 
 const addInput = ref<any>(null)
 
@@ -30,25 +35,40 @@ const addNewItem = (e: Event) => {
 <template>
     <h1>To-Do List</h1>
 
-    <button v-if="!isAddMode" class="btn btn-primary w-100" @click="isAddMode = true">Add new item</button>
+    <div class="form-check mt-2">
+        <input id="input-hidecompleted" class="form-check-input" type="checkbox" v-model="hideCompleted" />
+        <label class="form-check-label" for="input-hidecompleted">Hide completed items ({{ completedCount }})</label>
+    </div>
 
-    <div v-else>
-        <form @submit="addNewItem">
-            <input class="mb-2 form-control" ref="addInput" v-model="newItemLabel" />
+    <hr />
 
-            <div class="btn-group w-100" role="group">
-                <button class="btn btn-success" :disabled="!newItemLabel" type="submit">Add</button>
-                <button class="btn btn-danger" type="reset" @click="isAddMode = false">Cancel</button>
-            </div>
-        </form>
+    <div>
+        <div v-if="!isAddMode">
+            <button class="btn btn-primary w-100" @click="isAddMode = true">Add new item</button>
+        </div>
+
+        <div v-else>
+            <form @submit="addNewItem">
+                <input class="mb-2 form-control" ref="addInput" placeholder="New item" v-model="newItemLabel" />
+
+                <div class="btn-group w-100" role="group">
+                    <button class="btn btn-success" :disabled="!newItemLabel" type="submit">Add</button>
+                    <button class="btn btn-danger" type="reset" @click="isAddMode = false">Cancel</button>
+                </div>
+            </form>
+        </div>
     </div>
 
     <div class="mt-2">
-        <div v-if="items.length <= 0">No items!</div>
+        <div v-if="items.length <= 0">No items to show!</div>
 
-        <div v-else class="form-check" v-for="(item, idx) in items">
+        <div v-else class="form-check" v-for="(item, idx) in itemsToShow">
             <input :id="'input-' + idx" class="form-check-input" type="checkbox" v-model="item.done" />
             <label class="form-check-label" :for="'input-' + idx">{{ item.label }}</label>
+        </div>
+
+        <div v-if="hideCompleted" class="text-muted">
+            {{ completedCount }} completed item(s)
         </div>
     </div>
 </template>
